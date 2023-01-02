597eeccf1bed94b2f1100d8403db666d5b9a99a1
Mike DiDomizio <mikedidomizio@gmail.com>
2023-01-02 12:14:23 -0500

    Gcloud test

--- src/components/ui/search/SearchResult.svelte
+++ src/components/ui/search/SearchResult.svelte
@@ -50,6 +50,21 @@
     } else {
       $favorites = [...$favorites, title];
     }
+
+    const hashCode = function(str) {
+      var hash = 0,
+        i, chr;
+      if (str.length === 0) return hash;
+      for (i = 0; i < str.length; i++) {
+        chr = str.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+      }
+      return hash;
+    }
+
+    const str = 'revenue'
+    console.log($favorites.sort(), encodeURIComponent($favorites.sort().join(' ')))
   }
 
   function copyToClipboard() {
